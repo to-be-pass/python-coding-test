a35846a7c514275126ade1b1fe99951aca1a9066
tiaz0128 <tiaz0128.dev@gmail.com>
2024-01-21 08:33:28 +0900

    fix

--- src/tiaz0128/ch_05/solution_005.py
+++ src/tiaz0128/ch_05/solution_005.py
@@ -1,8 +1,11 @@
 def solution(arr1, arr2):
-    # 행렬
-    # arr1 = M * k
-    # arr2 = K * N
-    # result = M * N
+    # arr2 에서 각 열을 기준으로 계산 대상을 재구성
+    arr2_transposed = list(zip(*arr2))
+
+    result = []
     # for arr1 행
-    #  - for arr2 열
-    return
+    for row in arr1:
+        #  for arr2 열
+        result.append([sum(a * b for a, b in zip(row, col)) for col in arr2_transposed])
+
+    return result
