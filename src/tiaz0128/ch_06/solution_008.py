490aa9c1820a89f0488723a8f3ff72d06f6e1ddb
tiaz0128 <tiaz0128.dev@gmail.com>
2024-01-23 06:39:10 +0900

    passed

--- src/tiaz0128/ch_06/solution_008.py
+++ src/tiaz0128/ch_06/solution_008.py
@@ -1,7 +1,15 @@
 def solution(s):
-    # 1. 입력값이 ( 인 경우 스택에 넣고
-    # 2. 입력값이 ) 인 경우는 스택에 꺼낸다.
-    # 3. 최종적으로 스택에 값이 없으면 True
-    # 4. 예외적으로 스택에 아무런 값이 경우 입력값 ) 오는 경우는 False
+    stack = []
+    for ch in s:
+        # 4. 예외적으로 스택에 아무런 값이 경우 입력값 ) 오는 경우는 False
+        if len(stack) == 0 and ch == ")":
+            return False
 
-    return
+        if ch == "(":  # 1. 입력값이 ( 인 경우 스택에 넣고
+            stack.append(ch)
+
+        else:  # 2. 입력값이 ) 인 경우는 스택에 꺼낸다.
+            stack.pop()
+
+    # 3. 최종적으로 스택에 값이 없으면 True
+    return len(stack) == 0
