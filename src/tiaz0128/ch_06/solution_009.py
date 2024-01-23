30e2b7afdf0f5a85de8f2ffbd0d975425c292189
tiaz0128 <tiaz0128.dev@gmail.com>
2024-01-23 20:15:51 +0900

    try-2

--- src/tiaz0128/ch_06/solution_009.py
+++ src/tiaz0128/ch_06/solution_009.py
@@ -3,15 +3,15 @@ def solution(decimal):
     mock = decimal
     while True:
         # 2. 나눈 나머지를 스택에 저장한다.
-        stack.append(str(mock % 2))
+        stack.append(mock % 2)
 
         # 1. 해당 숫자를 2로 계속 나눈다.
         mock = mock // 2
 
         # 3. 더이상 나눌수 없으면 반복을 멈춘다.
         if mock < 2:
+            stack.append(mock)
             break
 
     # 4. 스택에 저장한 값을 거꾸로 적는다.
-    a = reversed(stack)
-    return "".join(a)
+    return "".join(map(str, stack[::-1]))
