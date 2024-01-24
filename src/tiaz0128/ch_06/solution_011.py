d92a65968d8290067f3380528b66a381c6407d97
tiaz0128 <tiaz0128.dev@gmail.com>
2024-01-24 19:43:44 +0900

    try-1

--- src/tiaz0128/ch_06/solution_011.py
+++ src/tiaz0128/ch_06/solution_011.py
@@ -1,8 +1,20 @@
 def solution(s):
     # 0. 길이가 1이면 -> 0
-    # 1. 반복해서 문자열 끝가지 가고
-    # 2. 스택에 값을 넣고
-    # 3. 하나, 그다음 하나 를 확인 = 같은 경우 pop
-    # 4. 아닌 경우 계속해서
+    if len(s) < 2:
+        return 0
 
-    return
+    stack = []
+
+    # 1. 반복해서 문자열 끝가지 가고
+    for ch in s:
+        # 2. 스택에 값을 넣고
+        if not stack:
+            stack.append(ch)
+
+        # 3. 하나, 그다음 하나 를 확인 = 같은 경우 pop
+        elif stack[-1] == ch:
+            stack.pop()
+        else:
+            stack.append(ch)
+
+    return 1 if not stack else 0
