e7a1abfa76c70db6604426d3eafb6f67d8cde10a
tiaz0128 <tiaz0128.dev@gmail.com>
2024-01-25 23:51:26 +0900

    try-2

--- src/tiaz0128/ch_06/solution_012.py
+++ src/tiaz0128/ch_06/solution_012.py
@@ -1,17 +1,9 @@
 def solution(prices):
-    answer = []
-    # 1. prices 를 반복하면서 기준이 되는 start 를 하나씩 증가
-    for idx, price in enumerate(prices, 1):
-        # 2. start 에서 그 이후에 있는 값들과 비교
-        cnt = 0
-        for compare_price in prices[idx:]:
-            # 4. 단, 바로 다음에 떨어지더라도 1초로 시작 = 기록하는 값은 1초로 시작
-            cnt += 1
-            # 3. 비교는 기준 start 보다 비교값이 더 작은경우에
-            if compare_price >= price:
-                continue
-            else:
-                break
-        answer.append(cnt)
+    # O(N^2)
 
-    return answer
+    # 1. 주식 가격이 떨어지지 않으면 계속 스택에 인덱스값을 저장한다.
+    # 2. 주식 가격이 떨어지면 스택에 저장되어 있는 인덱스의 값들을 확인하고
+    # 3. 떨어지는 값보다 인덱스에 있는 값이 더 큰 경우
+    # 4. 답을 입력하고 POP
+    # 5. 마지막까지 남아 있는값들은 len(prices) - index값 - 1
+    return
