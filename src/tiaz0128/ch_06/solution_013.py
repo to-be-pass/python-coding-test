63c877882f4722fdfce8cac044c2b93698f9c26f
tiaz0128 <tiaz0128.dev@gmail.com>
2024-01-26 21:42:29 +0900

    fix

--- src/tiaz0128/ch_06/solution_013.py
+++ src/tiaz0128/ch_06/solution_013.py
@@ -11,6 +11,25 @@
 
 def solution(board, moves):
     # 1. 주어진 형태를 좀더 쉽게 풀기 위해 zip 을 쓰자
+    game = list(map(list, zip(*board)))
+    stack = []
+    cnt = 0
+
     # 2. zip으로 만든 배열에서 0이 아닌값을 꺼내서 0으로 바꾼다
-    # 3. 꺼낸값과 stack 최상단 값을 확인하고 쌓던지 없앤다
-    return
+    for move in moves:
+        column = game[move - 1]
+
+        for i in range(len(column)):
+            # 0이 아닌 = 인형이 있으면
+            if doll := column[i]:
+                column[i] = 0
+                # stack에 최상단과 현재 인형이 같은 경우
+                if stack and stack[-1] == doll:
+                    stack.pop()
+                    # 인형 한번에 두개 없어지니까 +2
+                    cnt += 2
+                else:
+                    stack.append(doll)
+                break
+
+    return cnt
