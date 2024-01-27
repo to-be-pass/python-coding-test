d5adfb168f93e9cba4975ee184ccc2782e4d3f64
tiaz0128 <tiaz0128.dev@gmail.com>
2024-01-27 16:20:57 +0900

    try-1

--- src/tiaz0128/ch_06/solution_014.py
+++ src/tiaz0128/ch_06/solution_014.py
@@ -4,12 +4,36 @@
 
 
 def solution(n, k, cmd):
-    # 1. cmd 에서 명령어를 반복하면서 형태를 변경 배열?
-    # 2. 선택 위치를 잘 고려
-    # 3. 삭제 복구 = 스택사용해서 관리
-    #  - 삭제시 C
-    #    - 바로 아레 행
-    #    - 삭제된 행이 가장 마지막 행인 경우 -> 바로 윗 행
-    #  - 복구시 Z
-    #   - 현재 선택된 행은 그대로
-    return
+    dirs = {"U": -1, "D": 1}
+    stack = []
+
+    arr = ["O"] * n
+
+    for command in cmd:
+        print(f"k = {k} / command {command}")
+
+        if command == "C":
+            stack.append(k)
+            arr.pop(k)
+
+            if k == len(arr):
+                k -= 1
+            pass
+        elif command == "Z":
+            idx = stack.pop()
+
+            if idx <= k:
+                k += 1
+
+            arr.insert(idx, "O")
+            pass
+        else:
+            dir, cnt = command.split(" ")
+            k += dirs[dir] * int(cnt)
+
+        print("k=", k)
+
+    for idx in reversed(stack):
+        arr.insert(idx, "X")
+
+    return "".join(arr)
