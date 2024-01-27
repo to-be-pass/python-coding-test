105bd020f2a7da6e01e584f4aa9d34c04b92f418
youngrongoh <youngrong.oh@gmail.com>
2024-01-27 21:41:50 +0900

    ch_06-014. 표 편집

---
+++ src/youngrongoh/ch_06/solution_014.py
@@ -0,0 +1,24 @@
+def solution(n, k, cmd):
+    table = [0] * n
+    pointer = k
+    delete_stack = []
+    for c in cmd:
+        if c[0] == 'U':
+            pointer -= int(c[2:])
+        elif c[0] == 'D':
+            pointer += int(c[2:])
+        elif c[0] == 'C':
+            table.pop(pointer)
+            delete_stack.append(pointer)
+            if len(table) == pointer:
+                pointer -= 1
+        else:
+            top = delete_stack.pop()
+            table.insert(top, 0)
+            if top <= pointer:
+                pointer += 1
+    while delete_stack:
+        top = delete_stack.pop()
+        table.insert(top, 1)
+
+    return ''.join(['O' if x == 0 else 'X' for x in table])
