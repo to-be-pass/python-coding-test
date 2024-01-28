7f468f4ea5656d1d5d39d69be7b83004da610d98
youngrongoh <youngrong.oh@gmail.com>
2024-01-28 10:21:16 +0900

    ch_07-016. 기능 개발 - 분석 참고

--- src/youngrongoh/ch_07/solution_016.py
+++ src/youngrongoh/ch_07/solution_016.py
@@ -1,14 +1,20 @@
 from collections import deque
 def solution(progresses, speeds):
-    queue = deque([[progresses[i], speeds[i]] for i in range(len(progresses))])
+    queue = deque([[progresses[i], speeds[i], i] for i in range(len(progresses))])
     result = []
+    front = 0
+    rear = len(queue) - 1
+    deploy = 0
     while queue:
-        for i in range(len(queue)):
-            queue[i][0] += queue[i][1]
-        deploy = 0
-        while queue and queue[0][0] >= 100:
+        item = queue.popleft()
+        item[0] += item[1]
+        if item[0] >= 100 and item[2] == front:
+            front += 1
             deploy += 1
-            queue.popleft()
-        if deploy > 0:
-            result.append(deploy)
+        else:
+            queue.append(item)
+        if item[2] == rear:
+            if deploy > 0:
+                result.append(deploy)
+            deploy = 0
     return result
