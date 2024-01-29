599932997d842a97426832ae0fd1438ab4a28a9a
tiaz0128 <tiaz0128.dev@gmail.com>
2024-01-30 00:12:59 +0900

    init

--- src/tiaz0128/ch_07/solution_016.py
+++ src/tiaz0128/ch_07/solution_016.py
@@ -1,2 +1,25 @@
+from collections import deque
+
+
 def solution(progresses, speeds):
-    return
+    pq = deque(zip(progresses, speeds))
+    stack = []
+
+    while pq:
+        for idx, (pro, speed) in enumerate(pq):
+            pq[idx] = (pro + speed, speed)
+
+        cnt = 0
+        for item in pq:
+            if item[0] >= 100:
+                cnt += 1
+            else:
+                break
+
+        if cnt > 0:
+            for _ in range(cnt):
+                pq.popleft()
+
+            stack.append(cnt)
+
+    return stack
