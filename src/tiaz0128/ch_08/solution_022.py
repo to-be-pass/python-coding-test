0550dc658caecb568d58f461c961b713861c8e4a
tiaz0128 <tiaz0128.dev@gmail.com>
2024-02-04 20:12:14 +0900

    init

--- src/tiaz0128/ch_08/solution_022.py
+++ src/tiaz0128/ch_08/solution_022.py
@@ -1,2 +1,16 @@
 def solution(record):
-    return
+    answer = []
+    users = {}
+
+    command_string = {"Enter": "님이 들어왔습니다.", "Leave": "님이 나갔습니다."}
+
+    for row in record:
+        command, user_id, *alias = row.split(" ")
+
+        if command in ["Enter", "Change"]:
+            users[user_id] = alias[0]
+
+        if command in ["Enter", "Leave"]:
+            answer.append("{" + user_id + "}" + command_string[command])
+
+    return [row.format(**users) for row in answer]
