ccea87c75a5fa4bfd39c57aac8908b9937e8e2d6
tiaz0128 <tiaz0128.dev@gmail.com>
2024-02-04 17:21:38 +0900

    try-1

--- src/tiaz0128/ch_08/solution_020.py
+++ src/tiaz0128/ch_08/solution_020.py
@@ -1,2 +1,9 @@
 def solution(participant, completion):
-    return
+    a = {part: 0 for part in participant}
+    for x in participant:
+        a[x] += 1
+
+    for x in completion:
+        a[x] -= 1
+
+    return [x for x in a if a[x] != 0][0]
