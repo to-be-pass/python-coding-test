ed0aa498f061667ae3556fc705bb9aa4bb21a577
tiaz0128 <tiaz0128.dev@gmail.com>
2024-02-04 17:30:25 +0900

    try-1

--- src/tiaz0128/ch_08/solution_021.py
+++ src/tiaz0128/ch_08/solution_021.py
@@ -1,2 +1,21 @@
+from collections import Counter
+
+
 def solution(want, number, discount):
-    return
+    answer = 0
+
+    wanted_items = {want[idx]: cnt for idx, cnt in enumerate(number)}
+
+    for idx, item in enumerate(discount):
+        items = discount[idx : idx + 10]
+        counter = Counter(items)
+
+        if all(
+            [
+                cnt <= counter.get(category, False)
+                for category, cnt in wanted_items.items()
+            ]
+        ):
+            answer += 1
+
+    return answer
