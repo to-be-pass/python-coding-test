7d15dedfea70f2d0245ad73a736c8584c2c3d7dd
youngrongoh <youngrong.oh@gmail.com>
2024-02-05 22:31:48 +0900

    ch_08-026. 재귀로 풀기

--- src/youngrongoh/ch_09/solution_026.py
+++ src/youngrongoh/ch_09/solution_026.py
@@ -1,88 +1,26 @@
 def solution(nodes):
-    return [preorder_traverse(nodes), inorder_traverse(nodes), postorder_traverse(nodes)]
+    return [' '.join(preorder_traverse(nodes, 0)), ' '.join(inorder_traverse(nodes, 0)), ' '.join(postorder_traverse(nodes, 0))]
 
-def preorder_traverse(nodes):
-    length = len(nodes)
-    ptr = 0
-    visited = [False] * length
+def preorder_traverse(nodes, idx):
     result = []
-    while len(result) < length:
-        parent = round((ptr - 1.5) / 2)
-        left_idx = ptr * 2 + 1
-        right_idx = ptr * 2 + 2
+    if idx < len(nodes):
+        result.append(str(nodes[idx]))
+        result.extend(preorder_traverse(nodes, idx * 2 + 1))
+        result.extend(preorder_traverse(nodes, idx * 2 + 2))
+    return result
 
-        if not visited[ptr]:
-            visited[ptr] = True
-            result.append(nodes[ptr])
-
-        if left_idx < length and not visited[left_idx]:
-            ptr = left_idx
-            continue
-        elif right_idx < length and not visited[right_idx]:
-            ptr = right_idx
-            continue
-        elif parent >= 0:
-            ptr = parent
-        else:
-            break;
-    return ' '.join(map(str, result))
-
-def inorder_traverse(nodes):
-    length = len(nodes)
-    ptr = 0
-    visited = [False] * length
+def inorder_traverse(nodes, idx):
     result = []
-    while len(result) < length:
-        parent = round((ptr - 1.5) / 2)
-        left_idx = ptr * 2 + 1
-        right_idx = ptr * 2 + 2
+    if idx < len(nodes):
+        result.extend(inorder_traverse(nodes, idx * 2 + 1))
+        result.append(str(nodes[idx]))
+        result.extend(inorder_traverse(nodes, idx * 2 + 2))
+    return result
 
-        if left_idx < length and not visited[left_idx]:
-            ptr = left_idx
-            continue
-
-        if not visited[ptr]:
-            visited[ptr] = True
-            result.append(nodes[ptr])
-
-        if right_idx < length and not visited[right_idx]:
-            ptr = right_idx
-            continue
-
-        if parent >= 0:
-            ptr = parent
-            continue
-    return ' '.join(map(str, result))
-
-def postorder_traverse(nodes):
-    length = len(nodes)
-    ptr = 0
-    visited = [False] * length
+def postorder_traverse(nodes, idx):
     result = []
-    while len(result) < length:
-        parent = round((ptr - 1.5) / 2)
-        left_idx = ptr * 2 + 1
-        right_idx = ptr * 2 + 2
-
-        if left_idx < length and not visited[left_idx]:
-            ptr = left_idx
-            continue
-        elif right_idx < length and not visited[right_idx]:
-            ptr = right_idx
-            continue
-
-        if not visited[ptr]:
-            visited[ptr] = True
-            result.append(nodes[ptr])
-
-        right_of_parent = parent * 2 + 2
-        if right_of_parent < length and not visited[right_of_parent]:
-            ptr = right_of_parent
-            continue
-
-        if parent >= 0:
-            ptr = parent
-        else:
-            break;
-    return ' '.join(map(str, result))
-
+    if idx < len(nodes):
+        result.extend(postorder_traverse(nodes, idx * 2 + 1))
+        result.extend(postorder_traverse(nodes, idx * 2 + 2))
+        result.append(str(nodes[idx]))
+    return result
