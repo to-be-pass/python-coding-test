461edd42dc26674d4c4d42332b8275eacb8ad207
youngrongoh <youngrong.oh@gmail.com>
2024-02-06 11:35:46 +0900

    ch_09-028. 교재 답안 참고

--- src/youngrongoh/ch_09/solution_028.py
+++ src/youngrongoh/ch_09/solution_028.py
@@ -1,15 +1,9 @@
-import math
 def solution(N, A, B):
-    length = 2**(math.log(N, 2) + 1) - 1
-    idx_a = (length - 1) - (N - A)
-    idx_b = (length - 1) - (N - B)
-    curr_a = idx_a
-    curr_b = idx_b
-    count = 0
-    while curr_a != curr_b:
-        curr_a = round((curr_a - 1.5) / 2)
-        curr_b = round((curr_b - 1.5) / 2)
-        count += 1
-        if count == 10:
-            break;
-    return count
+    answer = 0
+    a = A
+    b = B
+    while a != b:
+        a = (a + 1) // 2
+        b = (b + 1) // 2
+        answer += 1
+    return answer
