d17f575345e5a8f062f62cabce466eee9b610fdd
tiaz0128 <tiaz0128.dev@gmail.com>
2024-02-10 08:34:57 +0900

    try-1

--- src/tiaz0128/ch_09/solution_029.py
+++ src/tiaz0128/ch_09/solution_029.py
@@ -1,2 +1,19 @@
+import math
+from collections import defaultdict
+
+
 def solution(enroll, referral, seller, amount):
-    return
+
+    inheritance = list(zip(enroll[::-1], referral[::-1]))
+
+    amount_by_seller = defaultdict(int)
+    for name, cnt in zip(seller, amount):
+        amount_by_seller[name] += 100 * cnt
+
+    for enr, ref in inheritance:
+        income = math.floor(amount_by_seller[enr] * 0.1)
+
+        amount_by_seller[enr] -= income
+        amount_by_seller[ref] += income
+
+    return [amount_by_seller.get(name, 0) for name in enroll]
