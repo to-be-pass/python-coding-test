4b24f731266354a6c0f9d07c4c0ad85cffc31f16
tiaz0128 <tiaz0128.dev@gmail.com>
2024-02-19 21:10:55 +0900

    try-1

--- src/tiaz0128/ch_10/solution_036.py
+++ src/tiaz0128/ch_10/solution_036.py
@@ -1,2 +1,8 @@
 def solution(phone_book):
-    return
+    phone_book.sort()
+
+    for idx, phone in enumerate(phone_book):
+        if idx != 0 and phone.startswith(phone_book[idx - 1]):
+            return False
+
+    return True
