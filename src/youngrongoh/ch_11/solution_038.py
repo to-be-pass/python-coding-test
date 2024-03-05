e665e80e7bc1991dbad50feac5c17ceac6ee1f20
youngrongoh <youngrong.oh@gmail.com>
2024-03-05 10:08:30 +0900

    ch_11-038. 깊이 우선 탐색 순회 - 재귀

--- src/youngrongoh/ch_11/solution_038.py
+++ src/youngrongoh/ch_11/solution_038.py
@@ -2,18 +2,21 @@ def solution(graph, start):
     nodes = {}
     for [a, b] in graph:
         if a not in nodes:
-            nodes[a] = { 'key': a, 'adj': [], 'visited': False }
+            nodes[a] = { 'adj': [], 'visited': False }
         if b not in nodes:
-            nodes[b] = { 'key': b, 'adj': [], 'visited': False }
+            nodes[b] = { 'adj': [], 'visited': False }
         nodes[a]['adj'].append(b)
     
-    stack = [start]
-    visit_order = []
-    while stack:
-        node = nodes[stack.pop()]
-        if node['visited']:
-            continue
-        node['visited'] = True
-        stack.extend(sorted(node['adj'], reverse=True))
-        visit_order.append(node['key'])
-    return visit_order
+    answer = []
+    dfs(nodes, start, answer)
+    return answer
+
+def dfs(nodes, curr, result):
+    if nodes[curr]['visited']:
+        return
+    else:
+        nodes[curr]['visited'] = True
+        result.append(curr)
+        for key in nodes[curr]['adj']:
+            dfs(nodes, key, result)
+        return
