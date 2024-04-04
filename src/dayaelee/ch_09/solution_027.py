f84c2b653ea614c5344e7c9713a27a34513e4d12
dayaelee <dayae.dev@gmail.com>
2024-04-04 15:07:33 +0900

    027

--- src/dayaelee/ch_09/solution_027.py
+++ src/dayaelee/ch_09/solution_027.py
@@ -1,65 +1,55 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+class BST:
+    def __init__(self):
+        self.root = None
+    
+    def insert(self, key):
+        if not self.root:
+            self.root = Node(key)
+        else:
+            curr = self.root
+            while 1:
+                if curr.val > key:
+                    if curr.left:
+                        curr = curr.left
+                    else:
+                        curr.left = Node(key)
+                        break
+                else:
+                    if curr.right:
+                        curr = curr.right
+                    else:
+                        curr.right = Node(key)
+                        break
+
+    def search(self, key):
+        curr = self.root
+        while curr and key != curr.val:
+            if curr.val > key:
+                curr = curr.left
+            else:
+                curr = curr.right
+        
+        return curr
+        
 def solution(lst, search_lst):
 
-    class Node:
-        # 노드 생성자
-        def __init__(self, key):
-            self.left = None
-            self.right = None
-            self.val = key
+    bst = BST()
 
-    # 이진 트리
-    class BST:
-        # 생성자
-        # 첨에 트리는 루트 자리만 주어짐 
-        def __init__(self):
-            self.root = None
-
-        # 넣기
-        def insert(self, key):
-            if not self.root:
-                self.root = Node(key) # 이런식으로도 객체 생성함
-            else:
-                curr = self.root
-                while 1:
-                    if curr.key > key:
-                        if curr.left:
-                            curr = curr.left
-                        else:
-                            curr.left = Node(key)
-                            break
-                    else:
-                        if curr.right:
-                            curr = curr.right
-                        else:
-                            curr.right = Node(key)
-                            break
-
-        # 찾기 
-        def search(self, k):
-            curr = self.root
-            while curr and curr.val != k:
-                if k < curr.val:
-                    curr = self.left # 이게 어딜 가리키는거? 
-                else:
-                    curr = self.right
-            return curr
-        
-            
-
-    bst = BST ()
-
-
+    for key in lst:
+        bst.insert(key)
+    
     result = []
 
-    for i in lst:
-        bst.insert(i)
-    
-    for j in search_lst:
-        if bst.search(j) == True:
+    for searchVal in search_lst:
+        if bst.search(searchVal):
             result.append(True)
         else:
             result.append(False)
-
-    print(result)
 
     return result
