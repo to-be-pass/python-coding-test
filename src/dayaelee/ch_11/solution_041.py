1a0a808efb352c70918862d4ff159f72c03ab838
dayaelee <dayae.dev@gmail.com>
2024-04-09 20:36:56 +0900

    041

---
+++ src/dayaelee/ch_11/solution_041.py
@@ -0,0 +1,28 @@
+def solution(graph, source):
+
+    vLen = len(graph)
+    distances = [float("inf")] * vLen
+    distances[source] = 0
+    predecessor = [None] * vLen
+
+    # 최단 경로 구하기 
+
+    for tmp in range(0, vLen-1):
+        for v, nodeSet in enumerate(graph):
+            for injeopNodeSet in nodeSet:
+                injeopNode, weight = injeopNodeSet
+                distance = distances[v] + weight
+                if distance < distances[injeopNode]:
+                    distances[injeopNode] = distance
+                    predecessor[injeopNode] = v
+    
+    # 음의 순환 구하기 
+                    
+    for v, nodeSet in enumerate(graph):
+        for injeopNodeSet in nodeSet:
+            injeopNode, weight = injeopNodeSet
+            distance = distances[v] + weight
+            if distance < distances[injeopNode]:
+                return [-1]
+
+    return [distances, predecessor]
