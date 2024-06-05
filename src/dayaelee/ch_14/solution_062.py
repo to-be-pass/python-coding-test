2e56bd88f8d6c062eba3c6b49b1ecd8ce7851ada
dayaelee <dayae.dev@gmail.com>
2024-06-05 16:42:59 +0900

    062

---
+++ src/dayaelee/ch_14/solution_062.py
@@ -0,0 +1,20 @@
+import copy
+def solution(arr, n):
+
+    maxN = len(arr)
+
+    tmp = [ ([0]*maxN) for i in range(maxN)]
+
+    while n> 0:
+        for i in range(maxN):
+            for j in range(maxN):
+                tmp[i][j] = arr[(maxN-1)-j][i]
+
+        arr = copy.deepcopy(tmp)
+        n-=1
+    
+
+
+    return tmp
+
+
